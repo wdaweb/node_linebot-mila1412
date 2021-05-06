011a67bfe9d4d2ee36617475b8e58db037fe1c52
mila1412 <k120240305@gmail.com>
2021-05-06 22:00:21 +0800

    獨立bubble

--- index.js
+++ index.js
@@ -37,216 +37,140 @@ bot.on('message', async event => {
     const result = data.filter(d => {
       return d.city === event.message.text && d.limited_time === 'no' && d.socket === 'yes'
     })
-    console.log(result)
+
+    // if (result.length > 4) {
+    //   const arr = []
+    //   for (const r of result) {
+    //     if (r.wifi === 5 && r.seat === 5) {
+    //       arr.push(r)
+    //     } else if (r.wifi === 5) {
+    //       arr.push(r)
+    //     }
+    //   }
+    //   result = arr
+    // }
 
     const flex = {
       type: 'carousel',
       contents: [
-        {
-          type: 'bubble',
-          size: 'micro',
-          body: {
-            type: 'box',
-            layout: 'vertical',
-            contents: [
-              {
-                type: 'text',
-                text: `${result[0].name}`,
-                weight: 'bold',
-                size: 'sm',
-                wrap: true
-              },
-              {
-                type: 'box',
-                layout: 'baseline',
-                contents: [
-                  {
-                    type: 'text',
-                    text: '4.0',
-                    size: 'xs',
-                    color: '#8c8c8c',
-                    flex: 0
-                  },
-                  {
-                    type: 'icon',
-                    size: 'xs',
-                    url: 'https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png',
-                    margin: 'xs',
-                    offsetTop: '1px'
-                  }
-                ]
-              },
-              {
-                type: 'box',
-                layout: 'vertical',
-                contents: [
-                  {
-                    type: 'box',
-                    layout: 'baseline',
-                    spacing: 'sm',
-                    contents: [
-                      {
-                        type: 'text',
-                        text: `${result[0].address}`,
-                        wrap: true,
-                        color: '#8c8c8c',
-                        size: 'xs',
-                        flex: 5,
-                        action: {
-                          type: 'uri',
-                          label: '123',
-                          uri: `http://maps.google.com/maps?q=loc:${encodeURI(result[0].latitude)},${encodeURI(result[0].longitude)}`
-                        }
-                      }
-                    ]
-                  }
-                ]
-              }
-            ],
-            spacing: 'sm',
-            paddingAll: '13px'
-          }
-        },
-        {
-          type: 'bubble',
-          size: 'micro',
-          body: {
-            type: 'box',
-            layout: 'vertical',
-            contents: [
-              {
-                type: 'text',
-                text: `${result[1].name}`,
-                weight: 'bold',
-                size: 'sm',
-                wrap: true
-              },
-              {
-                type: 'box',
-                layout: 'baseline',
-                contents: [
-                  {
-                    type: 'text',
-                    text: '4.0',
-                    size: 'xs',
-                    color: '#8c8c8c',
-                    flex: 0
-                  },
-                  {
-                    type: 'icon',
-                    size: 'xs',
-                    url: 'https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png',
-                    margin: 'xs',
-                    offsetTop: '1px'
-                  }
-                ]
-              },
-              {
-                type: 'box',
-                layout: 'vertical',
-                contents: [
-                  {
-                    type: 'box',
-                    layout: 'baseline',
-                    spacing: 'sm',
-                    contents: [
-                      {
-                        type: 'text',
-                        text: `${result[1].address}`,
-                        wrap: true,
-                        color: '#8c8c8c',
-                        size: 'xs',
-                        flex: 5,
-                        action: {
-                          type: 'uri',
-                          label: '123',
-                          uri: `http://maps.google.com/maps?q=loc:${encodeURI(result[1].latitude)},${encodeURI(result[1].longitude)}`
-                        }
-                      }
-                    ]
-                  }
-                ]
-              }
-            ],
-            spacing: 'sm',
-            paddingAll: '13px'
-          }
-        },
-        {
-          type: 'bubble',
-          size: 'micro',
-          body: {
-            type: 'box',
-            layout: 'vertical',
-            contents: [
-              {
-                type: 'text',
-                text: `${result[2].name}`,
-                weight: 'bold',
-                size: 'sm'
-              },
-              {
-                type: 'box',
-                layout: 'baseline',
-                contents: [
-                  {
-                    type: 'text',
-                    text: '4.0',
-                    size: 'xs',
-                    color: '#8c8c8c',
-                    flex: 0
-                  },
-                  {
-                    type: 'icon',
-                    size: 'xs',
-                    url: 'https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png',
-                    margin: 'xs',
-                    offsetTop: '1px'
-                  }
-                ]
-              },
-              {
-                type: 'box',
-                layout: 'vertical',
-                contents: [
-                  {
-                    type: 'box',
-                    layout: 'baseline',
-                    spacing: 'sm',
-                    contents: [
-                      {
-                        type: 'text',
-                        text: `${result[2].address}`,
-                        wrap: true,
-                        color: '#8c8c8c',
-                        size: 'xs',
-                        flex: 5,
-                        action: {
-                          type: 'uri',
-                          label: '123',
-                          uri: `http://maps.google.com/maps?q=loc:${encodeURI(result[2].latitude)},${encodeURI(result[2].longitude)}`
-                        }
-                      }
-                    ]
-                  }
-                ]
-              }
-            ],
-            spacing: 'sm',
-            paddingAll: '13px'
-          }
-        }
       ]
     }
+
+    const bubbles = []
+    for (const r of result) {
+      const name = r.name
+      const address = r.address
+      const latitude = r.latitude
+      const longitude = r.longitude
+      const b = {
+        type: 'bubble',
+        size: 'micro',
+        body: {
+          type: 'box',
+          layout: 'vertical',
+          contents: [
+            {
+              type: 'text',
+              text: `${name}`,
+              weight: 'bold',
+              size: 'sm',
+              wrap: true
+            },
+            {
+              type: 'box',
+              layout: 'baseline',
+              contents: [
+                {
+                  type: 'text',
+                  text: '4.0',
+                  size: 'xs',
+                  color: '#8c8c8c',
+                  flex: 0
+                },
+                {
+                  type: 'icon',
+                  size: 'xs',
+                  url: 'https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png',
+                  margin: 'xs',
+                  offsetTop: '1px'
+                }
+              ]
+            },
+            {
+              type: 'box',
+              layout: 'vertical',
+              contents: [
+                {
+                  type: 'box',
+                  layout: 'baseline',
+                  spacing: 'sm',
+                  contents: [
+                    {
+                      type: 'text',
+                      text: `${address}`,
+                      wrap: true,
+                      color: '#8c8c8c',
+                      size: 'xs',
+                      flex: 5,
+                      action: {
+                        type: 'uri',
+                        label: '123',
+                        uri: `http://maps.google.com/?q=${encodeURI(latitude)},${encodeURI(longitude)}`
+                      }
+                    }
+                  ]
+                }
+              ]
+            }
+          ],
+          spacing: 'sm',
+          paddingAll: '13px'
+        }
+      }
+      bubbles.push(b)
+    }
+    console.log(bubbles)
+    flex.contents = bubbles
+
     const message = {
       type: 'flex',
       altText: '今天要選哪一間咖啡廳呢？',
       contents: flex
     }
+
     fs.writeFileSync('aaa.json', JSON.stringify(message, null, 2))
     event.reply(message)
   }
 })
+
+// bot.on('message', async event => {
+//   if (event.message.type === 'location') {
+//     const distance = (lat1, lon1, lat2, lon2, unit) => {
+//       if (lat1 === lat2 && lon1 === lon2) {
+//         return 0
+//       } else {
+//         const radlat1 = (Math.PI * lat1) / 180
+//         const radlat2 = (Math.PI * lat2) / 180
+//         const theta = lon1 - lon2
+//         const radtheta = (Math.PI * theta) / 180
+//         let dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta)
+//         if (dist > 1) {
+//           dist = 1
+//         }
+//         dist = Math.acos(dist)
+//         dist = (dist * 180) / Math.PI
+//         dist = dist * 60 * 1.1515
+//         if (unit === 'K') {
+//           dist = dist * 1.609344
+//         }
+//         if (unit === 'N') {
+//           dist = dist * 0.8684
+//         }
+//         return dist
+//       }
+//     }
+//   }
+// })
 
 //  {
 //         type: 'location',
